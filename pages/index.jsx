1d6742cd4759a4bf00760f60ac44a6272c5a8b5d
oguuzduran <oguzduran@outlook.com.tr>
2022-12-08 15:09:27 +0300

    _document

--- pages/index.jsx
+++ pages/index.jsx
@@ -9,12 +9,7 @@ export default function Index() {
 				<link rel="icon" href="/favicon.ico" />
 				<link rel="preconnect" href="https://fonts.googleapis.com" />
 				<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-				<link
-					href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400;700&display=swap"
-					rel="stylesheet"
-				/>
 			</Head>
-			<h1 className="text-center bg-primary">Food Ordering</h1>
 			<Home />
 		</div>
 	);
