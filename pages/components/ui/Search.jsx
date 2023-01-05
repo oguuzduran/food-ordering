ef60fecda2619bb1452f11f51273ec7e570cf571
oguzdruan <oguzduran@outlook.com.tr>
2023-01-05 15:59:33 +0300

    header & hamburger menu & css fix

--- pages/components/ui/Search.jsx
+++ pages/components/ui/Search.jsx
@@ -13,7 +13,7 @@ const Search = ({ setIsSearchModal }) => {
 				}}
 			>
 				<div className="w-full h-full grid place-content-center relative">
-					<div className="relative z-50 w-[600px] h-[600px] bg-white border-2 p-5">
+					<div className="relative z-50 md:w-[600px] w-[370px] bg-white border-2 p-10 rounded-3xl">
 						<Title addClass="text-[40px] text-center">Search</Title>
 						<input type="text" placeholder="Search..." className="border w-full my-10" />
 						<div>
